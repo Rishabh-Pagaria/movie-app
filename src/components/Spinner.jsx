0a782a6ba06774fce62af718456af971e4012658
Rishabh-Pagaria <rpagaria2000@gmail.com>
2025-02-28 23:44:29 -0600

    adding the loader

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress';
+
+const Spinner = () => {
+  return (
+    <CircularProgress color="primary" />
+  )
+}
+
+export default Spinner
