9ab88844bc5d2cdf70d77cbd544e7f862a4a9f78
Rishabh-Pagaria <rpagaria2000@gmail.com>
2025-03-04 23:49:55 -0600

    movie card component render

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import starImg from "../assets/Rating.svg"
 
 const Movie = ({ movie: {title, vote_average, poster_path, release_date, original_language} }) => {
    return(
@@ -6,6 +7,16 @@ const Movie = ({ movie: {title, vote_average, poster_path, release_date, origina
         <img src={poster_path?`https://image.tmdb.org/t/p/w500/${poster_path}`:"/no-movie.png"} alt={title} />
         <div className="mt-4">
             <h3>{title}</h3>
+            <div className='content'>
+                <div className="rating">
+                    <img src={starImg} alt="rating" />
+                    <p>{vote_average ?  vote_average.toFixed(1) : "No rating"}</p>
+                </div>
+                <span>•</span>
+                <p className='lang'>{original_language}</p>
+                <span>•</span>
+                <p className='year'>{release_date ? release_date.split('-')[0] : "N/A"}</p>
+            </div>
         </div>
     </div>
   )
